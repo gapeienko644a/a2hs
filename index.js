f865385c5a5998a9ed388ff6ed6d6e020fe9d0e1
gapeienko644a <105996664+gapeienko644a@users.noreply.github.com>
2022-05-22 04:52:26 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -4,3 +4,22 @@ if ('serviceWorker' in navigator) {
     .register('/a2hs/sw.js')
     .then(() => { console.log('Service Worker Registered'); });
 }
+
+
+let EngUrl = "https://listenenglish.freevar.com/2021-November-December-phrases/phrases-of-the-day-practice-Nov-Decemb-22.htm";
+// var request = new XMLHttpRequest;
+// request.open("GET", EngUrl, true);
+// request.send();
+// let htmlcode;
+// request.onreadystatechange = function() {
+//   if (request.readyState == 4 && this.status == 200)
+//   htmlcode = request.responseText;
+//   alert(htmlcode);
+// };
+fetch(EngUrl)
+  .then((response) => {
+    return response.text;
+  })
+  .then((data) => {
+    console.log(data);
+  });
